a545b07dff569d2bc7856002ce61a3d3f7e7a0f0
Sherief Farid <sherief@farid.cloud>
2019-02-09 16:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector) {
+  var item
+  item = document.querySelector(selector)
+  return item
+}
+
+function nestedTarget() {
+  var item
+  item = document.querySelector("#nested .target")
+  return item
+}
+
+function deepestChild() {
+
+}
+
+function increaseRankBy() {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+}
